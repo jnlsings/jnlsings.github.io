f8deb6d808d698c996860570ba4aee6c24eb1a89
jnlsings <jnlsings@gmail.com>
2020-03-25 22:31:09 -0500

    Add resume/fb/email links

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar javascript from bulma docs
 document.addEventListener('DOMContentLoaded', () => {
 	// Get all "navbar-burger" elements
 	const $navbarBurgers = Array.prototype.slice.call(
